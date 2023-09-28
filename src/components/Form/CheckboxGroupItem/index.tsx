af2e7d3209e7c80d9f6d275b639bf5e13f6d8b40
TomasSadone <tomisadone27@gmail.com>
2023-09-28 12:21:45 -0300

    checkbox group

---
+++ src/components/Form/CheckboxGroupItem/index.tsx
@@ -0,0 +1,41 @@
+import RadioButton, { RadioButtonProps } from '../RadioButton';
+import style from './style.module.sass';
+
+type Props = {
+  description: string;
+  moreInformation?: string;
+  moreInformationOnClick?: (arg?: any) => void;
+  //name inside extra options should be diffrent from the one passed to the main radio button
+  extraOptions?: RadioButtonProps[];
+} & RadioButtonProps;
+
+const CheckboxGroupItem = ({
+  description,
+  moreInformation,
+  moreInformationOnClick,
+  extraOptions,
+  ...radioButtonProps
+}: Props) => {
+  return (
+    <div tabIndex={0}>
+      <div className={`${style.groupItem}`}>
+        <RadioButton {...radioButtonProps} />
+        <p>{description}</p>
+        {moreInformation && (
+          <button className={style.moreInformation} onClick={moreInformationOnClick}>
+            {moreInformation}
+          </button>
+        )}
+      </div>
+      {extraOptions && (
+        <div className={style.extraOptions}>
+          {extraOptions.map((option) => (
+            <RadioButton key={option.value} {...option} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CheckboxGroupItem;
