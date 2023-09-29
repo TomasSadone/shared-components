2ab4b77f2f82afebc7ab0065df2e360ff1877190
TomasSadone <tomisadone27@gmail.com>
2023-09-29 17:18:50 -0300

    extract tooltip logic into custom hook

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useRef, useState } from 'react';
 import style from './style.module.sass';
 import { createPortal } from 'react-dom';
+import useTooltip from './useTooltip';
 
 type Props = {
   children: ReactNode;
@@ -8,28 +9,7 @@ type Props = {
 };
 
 const Tooltip = ({ children, hoverItem }: Props) => {
-  const [coords, setCoords] = useState({});
-  const tooltipRef = useRef<HTMLDivElement>(null);
-  const handleMouseEnter = (e: React.MouseEvent<HTMLSpanElement>) => {
-    const rect = e.currentTarget.getBoundingClientRect();
-    const spaceAbove = rect.top;
-    const spaceBelow = window.innerHeight - rect.bottom;
-    const contentHeight = tooltipRef.current?.clientHeight
-      ? tooltipRef.current?.clientHeight
-      : 100;
-    const coordsToSet = {
-      left: rect.x,
-      top:
-        spaceAbove > spaceBelow
-          ? spaceAbove - contentHeight - 10
-          : spaceAbove + rect.height + 10,
-    };
-
-    setCoords(coordsToSet);
-
-    tooltipRef.current?.classList.add(style['show']);
-  };
-  const handleMouseLeave = () => tooltipRef.current?.classList.remove(style['show']);
+  const { coords, handleMouseEnter, handleMouseLeave, tooltipRef } = useTooltip();
 
   return (
     <div
