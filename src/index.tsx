b8494bd404dc46cd50730cb271b289a79f8288a0
TomasSadoneAigot <144695377+TomasSadoneAigot@users.noreply.github.com>
2023-12-15 08:13:21 -0300

    from babel & webpack to vite to simplify library creation (#14)
    * from babel & webpack to vite to simplify library creation
    
    * from babel & webpack to vite to simplify library creation
    
    ---------
    
    Co-authored-by: TomasSadone <tomisadone27@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 import './index.sass';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-    ,
-  </BrowserRouter>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
