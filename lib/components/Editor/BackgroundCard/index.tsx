b2ca533861b5f3191de55b28ee13074959bc679d
TomasSadone <tomisadone27@gmail.com>
2024-01-08 09:00:10 -0300

    make classname opitonal

--- lib/components/Editor/BackgroundCard/index.tsx
+++ lib/components/Editor/BackgroundCard/index.tsx
@@ -4,7 +4,12 @@ import style from './style.module.sass';
 type Props = {
   children: React.ReactNode;
   padding?: number;
+<<<<<<< HEAD
 };
+=======
+  className?: string;
+} & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+>>>>>>> 29609ce (make classname opitonal)
 
 export const BackgroundCard = ({ children, padding = 12 }: Props) => (
   <div style={{ padding: `${padding}px` }} className={style.card}>
