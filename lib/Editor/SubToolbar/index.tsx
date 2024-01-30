f45e9dde819f037b4b7024262303888eb4e05060
TomasSadone <tomisadone27@gmail.com>
2024-01-30 13:10:10 -0300

    [AIG-1716] Sub toolbar
    Component to pick mobile or desktop view for the template editor.
    
    - [x] Create the subtoolbar
    
    | Description         | value                                                              |
    | ------------------- | -------------------------------------------------------------------|
    | Type                | feature			                                   |
    | Ticket              | [AIG-1716](https://linear.app/aigot/issue/AIG-1716/create-sub-toolbar)|
    | Have tests          | :negative_squared_cross_mark:            			   |

---
+++ lib/Editor/SubToolbar/index.tsx
@@ -0,0 +1,43 @@
+import { BackgroundCard, Icon } from '../../main';
+import style from './style.module.sass';
+
+type Props = {
+  handleMobileView: () => void;
+  handleDesktopView: () => void;
+  defaultView: 'mobile' | 'desktop';
+  className?: string;
+};
+
+export const SubToolbar = ({
+  defaultView,
+  handleDesktopView,
+  handleMobileView,
+  className,
+}: Props) => (
+  <div className={[style.container, className].join(' ')}>
+    <BackgroundCard className={style.group} padding={0}>
+      <div className={`${defaultView === 'desktop' ? style.active : null}`}>
+        <Icon
+          onClick={handleDesktopView}
+          name="computer"
+          size={20}
+          viewBox="0 0 20 20"
+          fill="#667085"
+        />
+      </div>
+      <div className={`${defaultView === 'mobile' ? style.active : null}`}>
+        <Icon
+          onClick={handleMobileView}
+          name="smartphone"
+          size={20}
+          viewBox="0 0 20 20"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.67"
+          stroke="#667085"
+          fill="none"
+        />
+      </div>
+    </BackgroundCard>
+  </div>
+);
--- lib/components/Editor/BackgroundCard/index.tsx
+++ lib/components/Editor/BackgroundCard/index.tsx
@@ -4,10 +4,11 @@ import style from './style.module.sass';
 type Props = {
   children: React.ReactNode;
   padding?: number;
+  className?: string;
 };
 
-export const BackgroundCard = ({ children, padding = 12 }: Props) => (
-  <div style={{ padding: `${padding}px` }} className={style.card}>
+export const BackgroundCard = ({ children, padding = 12, className }: Props) => (
+  <div style={{ padding: `${padding}px` }} className={[style.card, className].join(' ')}>
     {children}
   </div>
 );
