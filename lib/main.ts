d51f461bed902ea6d5afbbc30ded58f463a89c70
TomasSadoneAigot <144695377+TomasSadoneAigot@users.noreply.github.com>
2024-02-15 09:20:15 -0300

    Aig 1734 (#20)
    * [AIG-1715] Editor's sidebar card.
    
    The base card of every sidebar component
    
    - [x] Create the card.
    
    | Description         | value                                                              |
    | ------------------- | -------------------------------------------------------------------|
    | Type                | feature 			                                   |
    | Ticket              | [AIG-1715](https://linear.app/aigot/issue/AIG-1728/background-card)|
    | Have tests          | :negative_squared_cross_mark:            			   |
    
    * export from library
    
    * [AIG-1734] Editor's sidebar image.
    
    - [x] Create the styled image component for the editor's sidebar.
    
    | Description         | value                                                              |
    | ------------------- | -------------------------------------------------------------------|
    | Type                | feature 			                                   |
    | Ticket              | [AIG-1733](https://linear.app/aigot/issue/AIG-1734/image)          |
    | Have tests          | :negative_squared_cross_mark:            			   |
    
    * br at eof
    
    ---------
    
    Co-authored-by: TomasSadone <tomisadone27@gmail.com>
    Co-authored-by: ppicatto-aigot <136796262+ppicatto-aigot@users.noreply.github.com>

--- lib/main.ts
+++ lib/main.ts
@@ -37,6 +37,7 @@ export { Tooltip } from './components/Tooltip';
 export { UnderlineButton } from './components/UnderlineButton';
 export { Toolbar } from './components/Editor/Toolbar';
 export { BackgroundCard } from './components/Editor/BackgroundCard';
+export { EditorImage } from './components/Editor/EditorImage';
 export { BGCardWithButton } from './components/Editor/BGCardWithButton';
 export { ColorSchemeViewer } from './components/Editor/ColorSchemeViewer';
 export { FontFamilyViewer } from './components/Editor/FontFamilyViewer';
